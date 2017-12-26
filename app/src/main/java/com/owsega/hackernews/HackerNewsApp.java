b78404a2afbf70045c44045f039099d0430372d9
seyi <owoeye.g.o@gmail.com>
2017-12-26 21:52:44 +0100

    Add access to Hackernews API using Retrofit

---
+++ app/src/main/java/com/owsega/hackernews/HackerNewsApp.java
@@ -0,0 +1,11 @@
+package com.owsega.hackernews;
+
+import android.app.Application;
+
+public class HackerNewsApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

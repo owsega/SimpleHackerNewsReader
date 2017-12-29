be04223e0c353ba7793e8bbde4daff4be0bf9866
seyi <owoeye.g.o@gmail.com>
2017-12-29 18:55:25 +0100

    add compareTo function to Post, to compare based on the time field

--- app/src/main/java/com/owsega/hackernews/data/model/Post.java
+++ app/src/main/java/com/owsega/hackernews/data/model/Post.java
@@ -2,10 +2,11 @@ package com.owsega.hackernews.data.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
 
-public class Post implements Parcelable {
+public class Post implements Parcelable, Comparable<Post> {
 
     public static final Parcelable.Creator<Post> CREATOR = new Parcelable.Creator<Post>() {
         public Post createFromParcel(Parcel source) {
@@ -86,4 +87,8 @@ public class Post implements Parcelable {
         dest.writeString(this.text);
     }
 
+    @Override
+    public int compareTo(@NonNull Post o) {
+        return this.time.compareTo(o.time);
+    }
 }
